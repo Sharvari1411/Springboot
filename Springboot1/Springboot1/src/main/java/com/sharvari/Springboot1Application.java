04b43e1f63dc7e206360987b51953a6afda7e5ce
Sharvari-Thombre <45871443+Sharvari1411@users.noreply.github.com>
2021-02-09 22:27:17 +0530

    Commit2

--- Springboot1/Springboot1/src/main/java/com/sharvari/Springboot1Application.java
+++ Springboot1/Springboot1/src/main/java/com/sharvari/Springboot1Application.java
@@ -2,12 +2,20 @@ package com.sharvari;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.CommandLineRunner;
+
 
 @SpringBootApplication
-public class Springboot1Application {
+public class Springboot1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springboot1Application.class, args);
+		SpringApplication app= new SpringApplication(Springboot1Application.class);
+		app.run(args);
+	}
+	
+	@Override
+	public void run (String...arg0) throws Exception{
+		System.out.println("Spring boot application created...........");
 	}
 
 }
